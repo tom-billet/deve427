cde3299bb85b561cb99413f8d849672f519b683d
m4x0u80 <max80ad@gmail.com>
2025-03-12 10:01:59 +0100

    products avec css

--- app/routes/user/productList.jsx
+++ app/routes/user/productList.jsx
@@ -12,24 +12,24 @@ const ProductList = () => {
 
   return (
     <div className="p-6">
-      <h1 className="text-3xl font-bold text-center mb-6">Liste des Produits</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <h1 className="h1">Liste des Produits</h1>
+      <div className="grid">
         {products.map((product) => (
-          <div key={product.id} className="p-4 shadow-lg rounded-lg flex flex-col bg-white">
-            <div className="flex flex-col items-center">
+          <div key={product.id} className="p-4">
+            <div className="flex">
               <img 
                 src={product.image} 
                 alt={product.name} 
-                className="w-32 h-32 object-cover mb-4 rounded"
+                className="img"
               />
-              <h2 className="text-xl font-bold text-center">{product.name}</h2>
-              <div className="text-center mt-2">
-                <p className="text-gray-600">{product.description}</p>
-                <p className="text-green-500 font-semibold mt-2">{product.price}€</p>
-                <p className="text-sm text-gray-500">Stock: {product.quantity}</p>
+              <h2 className="h2">{product.name}</h2>
+              <div className="descriptif">
+                <p className="description">{product.description}</p>
+                <p className="prix">{product.price}€</p>
+                <p className="stock">Stock: {product.quantity}</p>
               </div>
             </div>
-            <button className="mt-auto self-end bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Ajouter au panier</button>
+            <button className="ajoutPanier">Ajouter au panier</button>
           </div>
         ))}
       </div>
